6a8dd85c765f9370bd784c7d2a9ee5228976cf30
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2018-07-07 17:36:25 +0100

    First commit with an example to switch on and off

---
+++ examples/on-off.js
@@ -0,0 +1,37 @@
+/**
+ * Created by nherriot on 07/07/18.
+ */
+
+var Blinkt = require('./Blinkt');
+// Create a blinkt object and set the lights onto a known OFF state
+leds = new Blinkt();
+leds.setup();
+leds.clearAll();
+leds.sendUpdate();
+let onOff = false;							// A variable to set when a light is on or off
+
+function switchOnOff() {
+	console.log('Switching on all lights');
+	if (onOff){
+		console.log('*** Switching Lights Off ***');
+		leds.clearAll();
+		onOff = false;
+	} else {
+		console.log('*** Switching Lights On ***');
+		leds.setAllPixels(156, 156, 156, 0.9);
+		onOff = true;
+	}
+	leds.sendUpdate();
+}
+
+function switchOff() {
+	console.log('Switching off all lights')
+	leds.clearAll();
+	leds.sendUpdate();
+}
+
+// Set an interval timer which switches the LED lights on every 5 seconds
+const intervalObjOnOff = setInterval(switchOnOff, 5000);
+
+// A hack to listen on stdin that will never come to keep our process alive.
+process.stdin.resume();
