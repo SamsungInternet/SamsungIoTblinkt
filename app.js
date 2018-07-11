a253d483f8fe26cc6ba8c8681e93d2292937a61d
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2018-07-11 13:43:56 +0100

    changing port address to 8080 on the light to get round the wifi router issues.

--- app.js
+++ app.js
@@ -140,7 +140,7 @@ function runServer() {
 
   // If adding more than one thing, use MultipleThings() with a name.
   // In the single thing case, the thing's name will be broadcast.
-  const server = new WebThingServer(new SingleThing(thing), 8888);
+  const server = new WebThingServer(new SingleThing(thing), 8080);
 
   process.on('SIGINT', () => {
     server.stop();
