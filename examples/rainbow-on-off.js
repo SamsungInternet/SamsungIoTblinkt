38a722889f40f4e4f72aade7dc2745365238c793
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2019-03-22 16:12:53 +0000

    adding changes to make the light work better with the WiFi manager

---
+++ examples/rainbow-on-off.js
@@ -0,0 +1,78 @@
+/**
+ * Created by nherriot on 07/07/18.
+ *
+ * A simple light which shows a moving rainbow of colour.
+ * To run:    /> node rainbow.js
+ *
+ */
+
+var convert = require('color-convert');
+var Blinkt = require("node-blinkt");
+
+// Creates 16 equal divisions of 360 degrees. (i.e. 22deg). Used to have an equal color 'hue' for each led
+var spacing = 360.0 / 16.0;
+const pixels = 8;                             // The number of pixels in our light
+const brightness = 1;                         // Set this to globally set the rainbow
+const acceleration = 2;                       // How quickly you want the rainbow to move 1 is per second 2 is twice as fast.
+
+// Create a blinkt object and set the lights onto a known OFF state
+leds = new Blinkt();
+leds.setup();
+leds.clearAll();
+leds.sendUpdate();
+
+// A function that will  take number of pixels to set and a brightness then create a range of colours based
+// on a start point on current UTC time and move through all the colours using a hue saturation and value/brightness
+function setRainbow(numPixels, brightness) {
+  // We need to start our colour hue where our 'seconds' hand is on our clock. e.g. 30sec = 180 deg. 15 sec = 45 deg
+  var currentDate = new Date();
+  var seconds = currentDate.getSeconds()* acceleration;
+  var currentHue = (seconds*6)%360;
+  //console.log('\n*** Current seconds time is: ' + currentDate.getSeconds() + '***');
+
+  for (x = 0; x < numPixels; x++) {
+    var offset = x * spacing;
+    // We need the radial number i.e. (0-360) to be in a range. If our calculated number is above 360 then take the remainder
+    var h = ((currentHue + offset) % 360);
+    //console.log('hue value is: ' + h);
+    var rgbArray = convert.hsv.rgb(h, 100, 100);
+    //console.log('RGB Array is: ' + rgbArray);
+    leds.setPixel(x, rgbArray[0], rgbArray[1], rgbArray[2], brightness);
+  }
+  leds.sendUpdate();
+}
+
+console.log('***** Starting Rainbow Light *****');
+// Set an interval timer which switches the LED lights on every 5 seconds
+var intervalObjOnOff = setInterval(setRainbow, 1000, pixels, brightness);
+
+
+function stopRainbowLight(arg) {
+  console.log(`Rainbw Light Stopped => ${arg}`);
+  clearInterval(intervalObjOnOff);
+  leds.setup();
+  leds.clearAll();
+  leds.sendUpdate();
+
+}
+
+setTimeout(stopRainbowLight, 20000, 'setTimeout called');
+
+module.exports = {
+  foo: function () {
+    // whatever
+  },
+  bar: function () {
+    // whatever
+  },
+
+  startRainbowLight: function() {
+    console.log('** Starting Rainbow Light **');
+    intervalObjOnOff = setInterval(setRainbow, 1000, pixels, brightness);
+    setTimeout(stopRainbowLight, 20000, 'setTimeout called');
+
+  }
+
+
+
+};
