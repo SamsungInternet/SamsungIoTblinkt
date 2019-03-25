0e70bb634ae91dda9f26a74533613f120bda1e11
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2019-03-25 14:29:28 +0000

    Removing libraries that are not required for our blog artilce and part of the Mozilla Public License 2.0

--- index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  Action: require('./lib/action'),
-  Event: require('./lib/event'),
-  Property: require('./lib/property'),
-  Thing: require('./lib/thing'),
-  Value: require('./lib/value'),
-  ...require('./lib/server'),
-};
